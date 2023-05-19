e9f9f0f0f1fb2498a4db310bd3301e4c2b61a3b6
Venenosking <513690298@qq.com>
2023-05-19 15:31:42 +0800

    update createplot

---
+++ 操作角色管理.py
@@ -0,0 +1,15 @@
+
+import os
+file_name = os.path.basename(__file__)[2:-3]
+
+p_role = {key + ' ('+file_name+')': value for key, value in {
+    
+   "创建角色":"create role 角色名@主机名;",
+   "删除角色":"drop role 角色名称@主机名;",
+   "查看角色":"select * from mysql.user;",
+   "查看当前角色":"select current_role();",
+   "角色分配给用户":"grant 角色名@主机名 to 用户名@主机名;",
+   "复制用户权限给其他用户":"grant 用户名@主机名 to 用户名@主机名;",
+   "设置默认角色":"set default role 角色名@主机名 to 用户名@主机名;",
+
+}.items()}
