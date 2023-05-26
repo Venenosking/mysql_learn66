75ca2094f468a1e87c0c461bb5afaa4b0f7ae78f
Venenosking <513690298@qq.com>
2023-05-26 13:29:52 +0800

    update createplot

--- 操作分区表.py
+++ 操作分区表.py
@@ -5,11 +5,11 @@ file_name = os.path.basename(__file__)[2:-3]
 
 p_partition = {key + ' ('+file_name+')': value for key, value in {
     
-   "创建List分区表":"create table 表名 () partition by list(字段)(partition 分区名 values in (具体值1,具体值2),partition 分区名 values in (具体值1,具体值2,具体值3));",
+   "创建List分区表":"create table 表名 () partition by list(字段)(partition 分区名 values in (具体值1,具体值2));",
    "额外添加一个List分区":"alter table 表名 add partition (partition 分区名 values in (具体值1,具体值2,...));",
    "额外删除一个List分区":"alter table 表名 drop partition 分区名;",
-   "重新定义List分区":"alter table 表名 reorganize partition 旧分区名1,旧分区名2 into (partition 新分区名1 values in (具体值1,具体值2),partition 新分区名2 values in (具体值3,具体值4));",
-   "创建range分区表":"create table 表名 () partition by range (字段)(partition 分区名1 values less than (具体值),partition 分区名2 values less than (具体值2));",
+   "重新定义List分区":"alter table 表名 reorganize partition 旧分区名1,旧分区名2 into (partition 新分区名1 values in (具体值1,具体值2));",
+   "创建range分区表":"create table 表名 () partition by range (字段)(partition 分区名1 values less than (具体值));",
    "查看分区表的数据分布":"select * from information_schema.partitions where table_schema=schema() and table_name='表名';",
    "额外添加一个range分区":"alter table 表名 add partition (partition 分区名 values less than (具体值));",
    "删除一个range分区":"alter table 表名 drop partition 分区名;",
