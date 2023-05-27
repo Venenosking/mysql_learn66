3def527f96118c4d05643afa5e219f0fae0098d3
Venenosking <513690298@qq.com>
2023-05-27 13:33:03 +0800

    update createplot

--- 生成语音.py
+++ 生成语音.py
@@ -1,21 +1,37 @@
-# 51 47
+html_content = """
+<!DOCTYPE html>
+<html>
+<head>
+	<title>播放MP3文件</title>
+</head>
+<body>
+	<audio id="myAudio" controls>
+		<source src="测试.mp3" type="audio/mpeg">
+		Your browser does not support the audio element.
+	</audio>
 
+	<button onclick="playAudio()">播放</button>
+	<button onclick="pauseAudio()">暂停</button>
 
-import pygame
+	<script>
+		var audio = document.getElementById("myAudio");
 
-# 初始化pygame
-pygame.init()
+		function playAudio() {
+			audio.play();
+		}
 
-# 加载mp3文件
-pygame.mixer.music.load('测试.mp3')
+		function pauseAudio() {
+			audio.pause();
+		}
+	</script>
+</body>
+</html>
+"""
 
-# 播放mp3文件
-pygame.mixer.music.play()
+# 将HTML内容写入文件
+with open("index.html", "w") as f:
+    f.write(html_content)
 
-# 等待音频播放完毕
-while pygame.mixer.music.get_busy():
-    continue
-
-# 退出pygame
-pygame.quit()
-
+# 将MP3文件复制到HTML文件所在的目录
+import shutil
+# shutil.copyfile("测试.mp3", "index_files/测试.mp3")
