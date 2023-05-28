f4151f285927d73cdbea713e9f8739d5d46ae942
Venenosking <513690298@qq.com>
2023-05-28 16:01:36 +0800

    update createplot

--- 操作用户管理.py
+++ 操作用户管理.py
@@ -4,7 +4,7 @@ file_name = os.path.basename(__file__)[2:-3]
 
 def p_users():
     p_user = {key + ' (' + file_name + ')': value for key, value in {
-        "创建用户": "create user 用户名@主机名 identified by 密码;",
+        "创建用户": "create user 用户名@主机名 identified by '密码';",
         "删除用户": "drop user 用户名@主机名;",
         "修改用户名或主机名": "先 mysql -u 用户名 -p 切换到当前用户,然后rename user 旧用户名@旧主机名 to 新用户名@新主机名 ;",
         "修改密码": "alter user 用户名@主机名 identified by '新密码';",
--- 生成语音.py
+++ 生成语音.py
@@ -13,6 +13,8 @@ def ai_reader_value(string):
             new_string += " 右括号 "
         else:
             new_string += char
+    # 把' 替换成引号
+    new_string = new_string.replace("'", " 引号 ")
     # 合并多个空格
     new_string = re.sub(r'\s+', ' ', new_string)
     # 字符末尾是  s的替换成寺 t替换成忑 d替换成的  
@@ -20,8 +22,9 @@ def ai_reader_value(string):
     new_string = new_string.replace("t ", "t忑 ")
     new_string = new_string.replace("d ", "d的 ")
     new_string = new_string.replace("/", "或者")
-    # 句中加400ms间隔时间 
-    new_string = new_string.replace(" ", "((⏱️=400))")
+    
+    # 句中加800ms间隔时间 
+    new_string = new_string.replace(" ", "((⏱️=800))")
     # 开头加1.5s间隔时间
     new_string = '((⏱️=1500))'+new_string
     # print("替换后的字符串为：", new_string)
