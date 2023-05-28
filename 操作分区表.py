8314cb43371a81c7abf6c5792afc390bbdc08be7
Venenosking <513690298@qq.com>
2023-05-28 14:01:01 +0800

    update createplot

--- 操作分区表.py
+++ 操作分区表.py
@@ -1,6 +1,31 @@
 
-import os
+import os,re
 file_name = os.path.basename(__file__)[2:-3]
+
+
+def AI_reader(string):
+   new_string = ""
+   # 替换左右括号
+   for char in string:
+      if char == "(":
+         new_string += " 左括号 "
+      elif char == ")":
+         new_string += " 右括号 "
+      else:
+         new_string += char
+   # 合并多个空格
+   new_string = re.sub(r'\s+', ' ', new_string)
+   # 字符末尾是  s的替换成寺 t替换成忑 d替换成的  
+   new_string = new_string.replace("s ", "s寺 ")
+   new_string = new_string.replace("t ", "t忑 ")
+   new_string = new_string.replace("d ", "d的 ")
+   # 句中加400ms间隔时间 
+   new_string = new_string.replace(" ", "((⏱️=400))")
+   # 开头加2s间隔时间
+   new_string = '((⏱️=2000))' + new_string
+
+   # print("替换后的字符串为：", new_string)
+   return new_string
 
 
 p_partition = {key + ' ('+file_name+')': value for key, value in {
@@ -21,6 +46,13 @@ p_partition = {key + ' ('+file_name+')': value for key, value in {
    "创建Key分区":"create table 表名 () partition by key(字段) partitions 数字;",
 
 }.items()}
+
+# AI_reader()
+# 去掉key中的()
+a_partition={re.sub(r'\([^)]*\)', '', key): value for key, value in p_partition.items()}
+for key, value in a_partition.items():
+    print(key, value)
+# print(a_partition)
 
 v_test={
    "额外添加一个Hash/Key分区 +":'alter table 表名1 add partition partitions 数字;',
--- 生成语音.py
+++ 生成语音.py
@@ -1,17 +1,33 @@
 # https://ttsmaker.com/zh-cn
 # 语速 0.85
+import re
 string = 'create table 表名 () partition by list(字段)(partition 分区名 values in (具体值));'
-new_string = ""
+# string = 'alter table 表名 add partition partitions 数字;'
+# string = 'alter table 表名 reorganize partition 旧分区名1 into (partition 新分区名1 values less than (具体值));'
 
-for char in string:
-    if char == "(":
-        new_string += " 左括号 "
-    elif char == ")":
-        new_string += " 右括号 "
-    else:
-        new_string += char
-
-# new_string = new_string.replace(" ", "((⏱️=400))",1)
-print("替换后的字符串为：", new_string)
-
+# value
+def AI_reader(string):
+    new_string = ""
+    # 替换左右括号
+    for char in string:
+        if char == "(":
+            new_string += " 左括号 "
+        elif char == ")":
+            new_string += " 右括号 "
+        else:
+            new_string += char
+    # 合并多个空格
+    new_string = re.sub(r'\s+', ' ', new_string)
+    # 字符末尾是  s的替换成寺 t替换成忑 d替换成的  
+    new_string = new_string.replace("s ", "s寺 ")
+    new_string = new_string.replace("t ", "t忑 ")
+    new_string = new_string.replace("d ", "d的 ")
+    # 句中加400ms间隔时间 
+    new_string = new_string.replace(" ", "((⏱️=400))")
+    # 开头加2s间隔时间
+    new_string = '((⏱️=2000))'+new_string
+    
+    print("替换后的字符串为：", new_string)
+    return new_string
+AI_reader(string)
 
